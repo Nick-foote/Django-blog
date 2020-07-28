89fd705db7bf0bfd30d94f25a3dfea51f5a09453
Nick-foote <68865367+Nick-foote@users.noreply.github.com>
2020-07-28 11:24:06 +0100

    Update settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0*u@5$686%jpd7f&)k0lm(f$oif&m(2q#t!vcwa0*+g(&p9om$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.177.62.50']
+ALLOWED_HOSTS = ['35.178.110.25']
 
 LOGIN_URL = '/login'
 
